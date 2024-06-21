56cd2c3afb4266c2e61a21aadf7dab9d7bb3c979
QB12345678 <phanb4963@gmail.com>
2024-06-21 20:54:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
+  theme: {
+    extend: {
+      colors: {
+        orange: '#ee4d2d'
+      }
+    }
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '80rem',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: '1rem',
+          paddingRight: '1rem'
+        }
+      })
+    })
+  ]
+}
